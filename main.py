4bfacc170c71b65ba3034b07a0c3e7d9bbb10eeb
Lasantha <lasantha1130@gmail.com>
2022-09-13 09:08:55 +0530

    add time to title

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 from flask import Flask, render_template
+from datetime import datetime
 
 app = Flask(__name__)
 
 
+# Current year
+# current_year = datetime.date.today().
+# current_month = datetime.date.today().month
+# current_day = datetime.date.today().day
+date = datetime.today().strftime("%d/%m/%Y")
+
+
 @app.route("/")
 def home():
-    return render_template("index.html")
+    print(date)
+    return render_template("index.html", date=date)
 
 
 if __name__ == "__main__":
