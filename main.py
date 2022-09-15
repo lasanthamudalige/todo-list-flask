b4ecfcc17aa2f61e2ed9ada13fbe3dbb97070c31
Lasantha <lasantha1130@gmail.com>
2022-09-16 00:06:46 +0530

    create database tables and tables for tasks and create delete new task function

--- main.py
+++ main.py
@@ -9,25 +9,28 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
-class new_task(db.Model):
+class NewTask(db.Model):
+    __tablename__ = "new_task"
     id = db.Column(db.Integer, primary_key=True, unique=True)
-    task = db.Column(db.String(250), nullable=False)
+    name = db.Column(db.String(250), nullable=False)
     time = db.Column(db.String(10), nullable=False)
     date = db.Column(db.String(10), nullable=False)
 
 
-class incomplete_task(db.Model):
+class ActiveTask(db.Model):
+    __tablename__ = "incomplete_task"
     id = db.Column(db.Integer, primary_key=True, unique=True)
-    task = db.Column(db.String(250), nullable=False)
+    name = db.Column(db.String(250), nullable=False)
     time = db.Column(db.String(10), nullable=False)
     date = db.Column(db.String(10), nullable=False)
     started = db.Column(db.String(10), nullable=False)
     completed = db.Column(db.String(10), nullable=False)
 
 
-class complete_task(db.Model):
+class CompleteTask(db.Model):
+    __tablename__ = "complete_task"
     id = db.Column(db.Integer, primary_key=True, unique=True)
-    task = db.Column(db.String(250), nullable=False)
+    name = db.Column(db.String(250), nullable=False)
     time = db.Column(db.String(10), nullable=False)
     date = db.Column(db.String(10), nullable=False)
     started = db.Column(db.String(10), nullable=False)
@@ -43,31 +46,38 @@ time = datetime.today().strftime("%H:%M:%S")
 
 @app.route("/")
 def home():
-    tasks = new_task.query.all()
-
-    # print(tasks[-1].name)
-    return render_template("index.html", date=date, tasks=new_task.query.all())
+    return render_template("index.html", date=date, tasks=NewTask.query.all())
 
 
-@app.route("/add_task", methods=["GET", "POST"])
-def add_task():
+@app.route("/add_new_task", methods=["GET", "POST"])
+def add_new_task():
     if request.method == "POST":
-        task = request.form.get("new-task")
+        task = request.form.get("new-task").title()
         print(task, time, date)
-        task = new_task(task=task, time=time, date=date)
+        task = NewTask(name=task, time=time, date=date)
         db.session.add(task)
         db.session.commit()
 
         return redirect("/")
 
 
-@app.route("/add_to_progress", methods=["GET", "POST"])
-def add_to_progress():
-    if request.method == "POST":
-        new_task = request.form.get("new-task")
-        print(new_task)
+@app.route("/delete_new_task/<int:task_id>")
+def delete_new_task(task_id):
+    task = NewTask.query.filter_by(id=task_id).first()
+    db.session.delete(task)
+    db.session.commit()
 
-        return redirect("/")
+    return redirect("/")
+
+
+@app.route("/add_to_progress/<int:task_id>")
+def add_to_progress(task_id):
+    task = NewTask.query.filter_by(id=task_id).first()
+    task_name = task.name
+
+    print(task.name)
+
+    return redirect("/")
 
 
 if __name__ == "__main__":
